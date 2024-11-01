d55558c17e009e54f2694ac31a8df6ac06763b14
Abhijith-TG <abhijithtg62@gmail.com>
2024-11-01 22:12:03 +0530

    learning node 1.1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //import and export only work if include "type":"module" 
 // in package.json
 import greet from './greet.js'
+//import multiple data
+import {p1,p2,p3} from './peoples.js'
 
 const name = "Sam";
 greet(name);
+
+console.log(p1,p2,p3);
---
+++ peoples.js
@@ -0,0 +1,6 @@
+
+    const p1 = "Snorlax";
+    const p2 = "Raichu";
+    const p3 = "Primeape";
+
+    export {p1,p2,p3};
