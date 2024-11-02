bee4c3a59e822d66ac9a39bb8ec17706fca76f8f
Abhijith-TG <abhijithtg62@gmail.com>
2024-11-02 18:20:11 +0530

    learning node 1.5 path parse())

--- learnPath.js
+++ learnPath.js
@@ -18,6 +18,17 @@ console.log(path.join("c:","new","user","file",".js","..",".."));
 //used for normalizing like removing unwanted \
 console.log(path.normalize("c:\\Users\\Desktop\\NodeJS\\LearningNode\\learnPath.js"));
 
+//parse() used for getting details about the path
+
+console.log(path.parse("c:\\Users\\Desktop\\NodeJS\\LearningNode\\learnPath.js"));
+
+//using the object name we can get alone values
+console.log(path.parse("c:\\Users\\Desktop\\NodeJS\\LearningNode\\learnPath.js").dir);
+
+console.log(path.parse("c:\\Users\\Desktop\\NodeJS\\LearningNode\\learnPath.js").name);
+
+
+
 
 
 
